f4d2210f6491ac4980ea143c1741306d6df93833
Thiago <thiago.fm2@hotmail.com>
2022-12-29 11:52:06 -0300

    add test db

--- main.py
+++ main.py
@@ -9,15 +9,22 @@ st.title("Solar Mapa Geográfico")
 
 m = leafmap.Map(center=[-15, -50], zoom=4)
 cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+root_regions = "https://github.com/tbrugz/geodata-br/blob/master/"
+regions_list = [
+    ("São Paulo", "geojson/geojs-35-mun.json"),
+    ("Rio de Janeiro", "geojson/geojs-33-mun.json"),
+]
 
-#m.add_geojson(regions, layer_name='US Regions')
+for region in regions_list:
+    m.add_geojson(root_regions+region[1], layer_name=f"Regiao {region[0]}")
+
+#m.add_geojson(regions, layer_name='Brazil Regions')
 m.add_points_from_xy(
     cities,
     x="longitude",
     y="latitude",
-    color_column='region',
-    icon_names=['gear', 'map', 'leaf', 'globe'],
+    color_column='region', #The icons length must be equal the unique values of color column
+    icon_names=['gear', 'map', 'leaf', 'globe'], #https://fontawesome.com/v4/icons/
     spin=True,
     add_legend=True,
 )
